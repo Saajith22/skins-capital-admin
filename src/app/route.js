452cb2f08230cf9fc937a3bf6f65d6d6dc9d901f
Sinista121 <mohammedsaajith93@gmail.com>
2024-06-03 00:22:54 +0400

    update

--- src/app/route.js
+++ src/app/route.js
@@ -1,8 +1,10 @@
+const allowed = ["164.92.120.155", "5.194.75.18", "146.190.158.54"];
+
 export async function GET(req) {
-  console.log("RAN ROUTE MFS");
-
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
-  console.log(ip, "IP MS");
+  console.log(ip, "IP OF USER");
+  if (!allowed.includes(ip)) return Response.redirect(`https://google.com`);
 
-  return Response.redirect("/dashboard");
+  const url = req.url.split("/").slice(0, 3).join("/");
+  return Response.redirect(`${url}/dashboard`);
 }
