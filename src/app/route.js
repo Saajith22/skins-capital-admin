fa3d61a4d8aa15fc3926c4a638c6b9e518910693
Sinista121 <mohammedsaajith93@gmail.com>
2024-06-03 08:44:59 +0400

    upd

--- src/app/route.js
+++ src/app/route.js
@@ -2,13 +2,13 @@ const allowed = [
   "164.92.120.155",
   "5.194.75.18",
   "146.190.158.54",
-  "64.23.228.248 ",
+  "99.99.66.240",
 ];
 
 export async function GET(req) {
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
   console.log(ip, "IP OF USER");
-  if (!allowed.includes(ip)) return Response.error();
+  if (!allowed.includes(ip)) return Response.error().status(404);
 
   const url = req.url.split("/").slice(0, 3).join("/");
   return Response.redirect(`${url}/dashboard`);
