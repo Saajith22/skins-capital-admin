c4181c2379acbd117d954ef1db1645dd41106c55
Sinista121 <mohammedsaajith93@gmail.com>
2024-06-03 08:41:39 +0400

    added augustus

--- src/app/route.js
+++ src/app/route.js
@@ -1,9 +1,14 @@
-const allowed = ["164.92.120.155", "5.194.75.18", "146.190.158.54"];
+const allowed = [
+  "164.92.120.155",
+  "5.194.75.18",
+  "146.190.158.54",
+  "64.23.228.248 ",
+];
 
 export async function GET(req) {
   const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(",")[0];
   console.log(ip, "IP OF USER");
-  if (!allowed.includes(ip)) return Response.redirect(`https://google.com`);
+  if (!allowed.includes(ip)) return Response.error();
 
   const url = req.url.split("/").slice(0, 3).join("/");
   return Response.redirect(`${url}/dashboard`);
